4ee7c8bea7dc5f5c8cdc16d7788b9129a90a1572
nhan3108 <nguyennhanytbbbb@gmail.com>
2025-06-21 21:06:09 +0700

    Fix Railway start command and dependencies

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def du_doan():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 1234))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
 
